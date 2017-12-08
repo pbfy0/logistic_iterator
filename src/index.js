178b8c76f09a73c37255ec6edaec2d211872d6fc
Paul F-Y <pbfy00@gmail.com>
2017-12-08 11:40:09 -0500

    new stuff

--- src/index.js
+++ src/index.js
@@ -27,24 +27,24 @@ class Axis {
 	}
 	
 	redraw() {
-		let grf = this.grf, f_max = this.f_max, f_min = this.f_min;
-		let w = f_max - f_min;
+		const grf = this.grf, f_max = this.f_max, f_min = this.f_min;
+		const w = f_max - f_min;
 		this.cnt.removeChildren();
 		this.cnt.addChild(grf);
 		grf.clear()
 		grf.beginFill(0x202020, 1)
-		let a = [Math.log10(5), Math.log10(2), 0]
-		let gradexp = -Math.log10(w / 10);
-		let ip = gradexp | 0, fp = gradexp % 1;
-		let rgxp = ip + a.find((x) => x <= fp)
-		let gradation = Math.pow(10, -rgxp);
-		let first = Math.ceil(f_min / gradation) * gradation;
-		let n = Math.floor(w / gradation);
+		const a = [Math.log10(5), Math.log10(2), 0]
+		const gradexp = -Math.log10(w / 10);
+		const ip = gradexp | 0, fp = gradexp % 1;
+		const rgxp = ip + a.find((x) => x <= fp)
+		const gradation = Math.pow(10, -rgxp);
+		const first = Math.ceil(f_min / gradation) * gradation;
+		const n = Math.floor(w / gradation);
 		console.log(gradation, first, n);
 		for(let i = 0, x = first; i < n; i++, x += gradation) {
-			let dxp = (x - f_min) * this.size / w;
+			const dxp = (x - f_min) * this.size / w;
 			this.drawRect(dxp, 0, 2, 10);
-			let txt = new PIXI.Text(x.toFixed(Math.ceil(rgxp)));
+			const txt = new PIXI.Text(x.toFixed(Math.ceil(rgxp)));
 			/*if(!this.vert) */txt.rotation = Math.PI / 8 - this.vert * Math.PI / 2;
 			this.setPos(txt.position, dxp, 12);
 			txt.anchor.y = 0.5
@@ -58,8 +58,8 @@ class Axis {
 
 
 class FunctionRenderer {
-	constructor({r_dim, f_dim, fn=f, skip_iters = 100, iters = 300, darkness = 0xff}) {
-		var rc = document.createElement('canvas');
+	constructor({r_dim, f_dim, fn=f, skip_iters = 1000, iters = 3000, darkness = 32}) {
+		let rc = document.createElement('canvas');
 		rc.width = r_dim.x;
 		rc.height = r_dim.y;
 		this.tex = PIXI.Texture.fromCanvas(rc);		
@@ -86,16 +86,16 @@ class FunctionRenderer {
 	redraw() {
 		const rect = this.f_dim
 		this.ctx.clearRect(0, 0, this.rc.width, this.rc.height);
-		var id = this.ctx.createImageData(this.rc.width, this.rc.height);
+		let id = this.ctx.createImageData(this.rc.width, this.rc.height);
 		const ww = this.rc.width, hh = this.rc.height;
 		const dk = this.darkness;
-		for(var i = rect.x; i <= rect.x+rect.width; i+=(rect.width/ww)) {
+		for(let i = rect.x; i <= rect.x+rect.width; i+=(rect.width/ww)) {
 			const it = this.fn(i);
 			const x = (i - rect.x) / rect.width;
 			const xx = (x * ww) | 0;
 			for(let j = 0; j < this.skip_iters; j++) it();
 			for(let j = 0; j < this.iters; j++) {
-				var v = it();
+				let v = it();
 				const y = (v - rect.y) / rect.height;
 				const yy = (y * hh) | 0;
 				if (yy < 0 || yy > ww) continue;
@@ -107,46 +107,72 @@ class FunctionRenderer {
 		this.tex.update();
 	}
 }
-	
-var app = new PIXI.Application(1920, 1080, {backgroundColor: 0xffffff});
-document.body.appendChild(app.view);
+const cc = new PIXI.Point(document.documentElement.clientWidth * window.devicePixelRatio, document.documentElement.clientHeight * window.devicePixelRatio);
+const app = new PIXI.Application(cc.x - 180, cc.y - 50, {backgroundColor: 0xffffff});
+document.getElementById('cc').appendChild(app.view);
+app.view.style.width = app.renderer.width / window.devicePixelRatio + "px"
 
-var frend = new FunctionRenderer({r_dim: new PIXI.Point(1920*2-200, 1080*2-200), f_dim: new PIXI.Rectangle(0, 0, 4, 1)});
-var gtx = frend.tex;
-var ww = frend.rc.width, hh = frend.rc.height;
-var spr = new PIXI.Sprite(gtx);
-spr.width = app.renderer.width - 100;
-spr.height = app.renderer.height - 100;
+const frw = (app.renderer.width - 100), frh = (app.renderer.height - 100);
+console.log(cc, app.renderer.width, app.renderer.height);
+const frend = new FunctionRenderer({r_dim: new PIXI.Point(frw * 2, frh * 2), f_dim: new PIXI.Rectangle(0, 0, 4, 1)});
+const gtx = frend.tex;
+const ww = frend.rc.width, hh = frend.rc.height;
+let container = new PIXI.Container();
+const spr = new PIXI.Sprite(gtx);
+let colorMatrix = new PIXI.filters.ColorMatrixFilter();
+colorMatrix.enabled = true;
+colorMatrix.negative();
+spr.filters = [colorMatrix];
+ window.qqq =colorMatrix;
+ window.ss = spr;
+
+//spr.width = frw
+//spr.height = frh
 spr.interactive = true;
 console.log(spr.scale.y);
 
-app.stage.addChild(spr);
+container.addChild(spr);
+container.scale.x = container.scale.y = 0.5;
+app.stage.addChild(container);
 app.stage.interactive = true;
 
-let h_axis = new Axis({f_min: 0, f_max: 4, vert: false, size: 1820});
-h_axis.cnt.position.y = 980;
+const h_axis = new Axis({f_min: 0, f_max: 4, vert: false, size: frw});
+h_axis.cnt.position.y = frh;
 app.stage.addChild(h_axis.cnt);
 h_axis.redraw();
-let v_axis = new Axis({f_min: 0, f_max: 1, vert: true, size: 980});
-v_axis.cnt.position.x = 1820;
+const v_axis = new Axis({f_min: 0, f_max: 1, vert: true, size: frh});
+v_axis.cnt.position.x = frw;
 app.stage.addChild(v_axis.cnt);
 v_axis.redraw();
 
 window.v_a = v_axis; window.h_a = h_axis;
 
-var tx = new PIXI.Text();
+const tx = new PIXI.Text();
 tx.position.x = 10;
 tx.position.y = 10;
 app.stage.addChild(tx);
 
-var initial_p = null;
-var initial_pg = null;
-var final_p = null;
-var gfx = new PIXI.Graphics();
+let initial_p = null;
+let initial_pg = null;
+let final_p = null;
+const gfx = new PIXI.Graphics();
 app.stage.addChild(gfx);
+
+const x0dom = document.getElementById('x0')
+const y0dom = document.getElementById('y0')
+const wdom = document.getElementById('w')
+const hdom = document.getElementById('h')
+function set_domwind() {
+	x0dom.value = frend.f_dim.x
+	y0dom.value = frend.f_dim.y
+	wdom.value = frend.f_dim.width
+	hdom.value = frend.f_dim.height
+}
+set_domwind();
 
 function set_wind(w) {
 	frend.f_dim = w;
+	set_domwind();
 	h_axis.f_min = w.x;
 	h_axis.f_max = w.x + w.width;
 	h_axis.redraw();
@@ -156,21 +182,23 @@ function set_wind(w) {
 	frend.redraw();
 }
 function upd_rect(pt, w, h) {
-	//var g = new PIXI.Graphics();
+	//let g = new PIXI.Graphics();
 	gfx.clear()
 	gfx.lineStyle(1, 0xff0000);
 	gfx.drawRect(pt.x, pt.y, w, h);
 }
 spr.on('pointerdown', (ev) => {
+	if(!ev.data.originalEvent.isPrimary) return;
 	if(initial_p != null) return;
 	initial_p = ev.data.getLocalPosition(spr);
 	initial_pg = ev.data.global.clone();
+	final_p = initial_p;
 	upd_rect(initial_pg, 0, 0);
 })
 spr.on('pointermove', (ev) => {
 	if(initial_p == null) return;
 	final_p = ev.data.getLocalPosition(spr);
-	var lc = ev.data.global;
+	let lc = ev.data.global;
 	upd_rect(initial_pg, lc.x - initial_pg.x, lc.y - initial_pg.y);
 })
 spr.on('pointermove', (ev) => {
@@ -179,18 +207,42 @@ spr.on('pointermove', (ev) => {
 	let y_exp = -Math.log10(frend.f_dim.height) + 3;
 	tx.text = '(' + pos.x.toFixed(x_exp) + ', ' + pos.y.toFixed(y_exp) + ')';
 })
+const label = document.getElementById('label')
+spr.on('rightclick', (ev) => { 
+	console.log(ev.data.originalEvent);
+	//ev.data.originalEvent.preventDefault();
+	const pos = frend.loc_to_fn(ev.data.getLocalPosition(spr))
+	const dd = document.getElementById('cis')
+	const x_exp = -Math.log10(frend.f_dim.width) + 5;
+	const y_exp = -Math.log10(frend.f_dim.height) + 5;
+	let ch;
+	while(ch = dd.lastChild) dd.removeChild(ch);
+	/*for(let i of dd.children) {
+		dd.removeChild(i);
+	}*/
+	const iter = frend.fn(pos.x);
+	label.textContent = 'Iteration at ' + pos.x.toFixed(x_exp);
+	for(let i = 0; i < frend.skip_iters; i++) iter();
+	for(let i = 0; i < frend.iters; i++) {
+		const c = document.createElement('li');
+		c.textContent = iter().toFixed(y_exp);
+		dd.appendChild(c);
+	}
+})
+app.view.addEventListener('contextmenu', (ev) => { ev.preventDefault(); return false; });
 let wind_queue = [];
 document.body.addEventListener('pointerup', (ev) => {
+	if(!ev.isPrimary) return;
 	if(initial_p == null) return;
-	var gd = final_p;
+	let gd = final_p;
 	console.log(gd.x, gd.y);
 
-	var x0 = Math.min(initial_p.x, gd.x) / ww;
-	var x1 = Math.max(initial_p.x, gd.x) / ww;
-	var y0 = Math.min(initial_p.y, gd.y) / hh;
-	var y1 = Math.max(initial_p.y, gd.y) / hh;
+	const x0 = Math.min(initial_p.x, gd.x) / ww;
+	const x1 = Math.max(initial_p.x, gd.x) / ww;
+	const y0 = Math.min(initial_p.y, gd.y) / hh;
+	const y1 = Math.max(initial_p.y, gd.y) / hh;
 	
-	var w = new PIXI.Rectangle(x0 * frend.f_dim.width + frend.f_dim.x, y0 * frend.f_dim.height + frend.f_dim.y, (x1 - x0) * frend.f_dim.width, (y1 - y0) * frend.f_dim.height);
+	let w = new PIXI.Rectangle(x0 * frend.f_dim.width + frend.f_dim.x, y0 * frend.f_dim.height + frend.f_dim.y, (x1 - x0) * frend.f_dim.width, (y1 - y0) * frend.f_dim.height);
 	
 	if(w.x + w.width / ww != w.x && w.height != 0) {
 		wind_queue.push(frend.f_dim);
@@ -220,3 +272,13 @@ document.getElementById('darkness').addEventListener('blur', (ev) => {
 document.getElementById('iters').addEventListener('blur', (ev) => {
 	frend.iters = parseInt(ev.target.value);
 })
+
+const aaa = {'x0': 'x', 'y0': 'y', 'w': 'width', 'h': 'height'}
+for(let k in aaa) {
+	const v = aaa		[k];
+	document.getElementById(k).addEventListener('blur', (ev) => {
+		console.log(v, ev.target.value);
+		frend.f_dim[v] = parseFloat(ev.target.value);
+		set_wind(frend.f_dim)
+	});
+}
